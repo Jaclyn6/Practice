4addee10ad0c25f80d329fec5147f465f34f1562
jaclyn <junwoo97@nate.com>
2018-11-24 03:08:28 +0900

    11/23-4

---
+++ defprac4.py
@@ -0,0 +1,7 @@
+def print_personal(name, position='staff', age=10):
+    print("name = {0}".format(name))
+    print("position = {0}".format(position))
+    print("age = {0}".format(age))
+
+print_personal('박상현','배우',15)
+print_personal('김성욱')
